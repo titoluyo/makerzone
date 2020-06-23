36c65197fbe9dd757975c74843d8dab41508785d
Tito Luyo Murata <tluyo@hotmail.com>
2020-06-22 20:32:55 -0500

    clean

---
+++ PlatformIO/MqttTemp/lib/TempMulti/TempMulti.h
@@ -0,0 +1,15 @@
+#ifndef TempMulti_h
+#define TempMulti_h
+
+class TempMulti
+{
+private:
+    /* data */
+public:
+    TempMulti(/* args */);
+    ~TempMulti();
+};
+
+
+
+#endif
--- PlatformIO/MqttTemp/lib/TempRead/TempRead.cpp
+++ PlatformIO/MqttTemp/lib/TempRead/TempRead.cpp
@@ -1,9 +1,6 @@
 #include "TempRead.h"
 
 DHT dht(DHTPIN, DHTTYPE);
-
-unsigned long nowTemp;
-unsigned long lastTemp = 0;
 char msgTemp[MSG_BUFFER_SIZE];
 
 void tempBegin() {
@@ -11,26 +8,13 @@ void tempBegin() {
 }
 
 const char* readTemp() {
-  nowTemp = millis();
-  snprintf (msgTemp, MSG_BUFFER_SIZE, "");
-  if (nowTemp - lastTemp > 2000) {
-    lastTemp = nowTemp;
-    float h = dht.readHumidity();
-    float t = dht.readTemperature(false);
-    if (isnan(h) || isnan(t)) {
-      Serial.println("Impossible to read DHT!");
-      return msgTemp;
-    }
-    snprintf (msgTemp, MSG_BUFFER_SIZE, "H:%1f,T%2fC", h, t);
-    /*
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.print(" *C ");
-    */
-   Serial.println(msgTemp);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(false);
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Impossible to read DHT!");
+    return "";
   }
-   return msgTemp;
+  snprintf (msgTemp, MSG_BUFFER_SIZE, "H:%1f,T%2fC", h, t);
+  Serial.println(msgTemp);
+  return msgTemp;
 }
